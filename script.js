6a44d77f6e89a60a1fa02a46f0a74fa2d6316ee3
Noisecode23 <gamerstark210@gmail.com>
2024-01-10 11:08:50 +0530

    project-12 files added to repository

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ async function getSongs() {
   for (let index = 0; index < as.length; index++) {
     const element = as[index];
     if (element.href.endsWith(".mp3")) {
-      songs.push(element.href);
+      songs.push(element.href.split("/songs/")[1]);
     }
   }
   return songs;
@@ -20,6 +20,22 @@ async function getSongs() {
 async function main() {
   //list of all songs
   let songs = await getSongs();
+  let songUL = document.querySelector('.songsList').getElementsByTagName('ul')[0]
+  for (const song of songs) {
+    songUL.innerHTML = songUL.innerHTML + `<li> 
+    <img class="invert" src="music.svg" alt="">
+    <div class="info">
+        <div>${song.replaceAll("%20"," ")}</div>
+        <div>Ankit</div>
+    </div>
+    <div class="playNow">
+        <span>play now</span>
+        <img class="invert" src="play2.svg" alt="">
+    </div>
+    
+    
+    </li>`
+  }
   console.log(songs);
 
   //play the first song
